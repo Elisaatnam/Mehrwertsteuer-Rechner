7af4d66de345adf5799b05d3b2a0a44b8e80fc3c
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-05-04 17:23:51 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,28 +22,38 @@ const calcMwSt = () => {
 	const EurBetrag = Number(document.querySelector("#betrag").value);
 	console.log(EurBetrag);
 
+	//rechnet d Mehrwertsteuerbetrag aus
 	const mWSt = Number(((EurBetrag / 100) * mwstSatz).toFixed(2));
 	console.log(mWSt);
 
+	// wenn Netto zu Brutto ausgewaehlt
 	if (bruOrNet == "n-zu-b") {
-		outputMwstBetrag.innerHTML = `${mWSt} Euro`;
-		total.innerHTML = `${EurBetrag + mWSt} Euro`;
-	} else {
-		outputMwstBetrag.innerHTML = `${mWSt} Euro`;
-		total.innerHTML = `${EurBetrag - mWSt} Euro`;
+		outputMwstBetrag.innerHTML = `${mWSt} €`;
+		total.innerHTML = `${EurBetrag + mWSt} €`;
+	}
+	//wenn Brutto zu Netto ausgewaehlt
+	else {
+		outputMwstBetrag.innerHTML = `${mWSt} €`;
+		total.innerHTML = `${EurBetrag - mWSt} €`;
 	}
 };
 
+// der Text muss sich aendern, wenn Brutto zu Netto ausgewaehlt ist
 const changeText = () => {
+	//same as in function calcMwSt, brauche ich um zu schauen welcher Radiobutton ausgewaehlt ist
 	const bruOrNet = document.querySelector(
 		'input[name="bru-nett"]:checked',
 	).value;
 
 	textAenderDich.innerHTML =
+		// ternary operator
 		bruOrNet == "n-zu-b"
-			? "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro"
-			: "Bruttobetrag (Preis inkl. Mehrwertsteuer) in Euro";
+			? //wenn true dann das
+			  "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro"
+			: //wenn false dann das
+			  "Bruttobetrag (Preis inkl. Mehrwertsteuer) in Euro";
 
 	textAenderDich2.innerHTML =
+		// nochmal ternary
 		bruOrNet == "n-zu-b" ? "Bruttobetrag (Endpreis)" : "Nettobetrag";
 };
